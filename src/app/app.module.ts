147483f7317250b2d5e9a5a374757e3c16e73712
Alan Jeria Medina <alanjeria01@gmail.com>
2024-08-13 14:43:26 -0400

    uppercase lowecase - tiltecase y ademas prime flex con Date pipe y cambiamos el idioma global

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,8 +7,18 @@ import { SharedModule } from './shared/shared.module';
 import { PrimeNgModule } from './prime-ng/prime-ng.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//configuracion del locale de la app
+
+import localesEsHN from '@angular/common/locales/es-HN'
+import localesEsFR from '@angular/common/locales/fr-CA'
+import localesEsCL from '@angular/common/locales/es-CL'
 
 
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localesEsHN);
+registerLocaleData(localesEsFR);
+registerLocaleData(localesEsCL);
 
 @NgModule({
   declarations: [
@@ -23,7 +33,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     BrowserModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID, useValue: 'es-HN'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
